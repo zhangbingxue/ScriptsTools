0eed808ebef7020fcfe0b4a9c4a81f156522d13d
zhangbingxue <1115450892@qq.com>
2021-11-02 14:49:59 +0800

    first

---
+++ demo.py
@@ -0,0 +1,414 @@
+from __future__ import unicode_literals
+
+import os
+
+from yaml import dump_all, load, FullLoader
+import image.image
+import ctypes
+import re
+import threading
+from datetime import datetime
+from time import sleep
+import serial.tools.list_ports
+# from PyQt5.QtCore import pyqtSlot, QDir, QFile, QIODevice, QTextStream, Qt
+import sys
+import matplotlib
+# Make sure that we are using QT5
+matplotlib.use('Qt5Agg')
+from PyQt5 import QtCore, QtWidgets
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import *
+from ui.mainwindow_ui import Ui_MainWindow
+flagConnect = False
+logginNum = 0
+
+class MyMplCanvas(FigureCanvas):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        self.compute_initial_figure()
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QtWidgets.QSizePolicy.Expanding,
+                                   QtWidgets.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+    def compute_initial_figure(self):
+        pass
+
+
+class MyStaticMplCanvas(MyMplCanvas):
+    """Simple canvas with a sine plot."""
+    def compute_initial_figure(self):
+        t = arange(0.0, 3.0, 0.01)
+        s = sin(2*pi*t)
+        self.axes.plot(t, s)
+
+class IctTest(QMainWindow, Ui_MainWindow):
+    infoMsgPrint = QtCore.pyqtSignal(str)
+    operMsgPrint = QtCore.pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super(IctTest, self).__init__(parent)
+        self.main_ui = Ui_MainWindow()
+        self.main_ui.setupUi(self)
+        self.initUI()
+
+    # 界面启动时初始化
+    def initUI(self):
+        # 增加左上角logo图标
+        self.setWindowIcon(QIcon(':/ICT.png'))
+        # 设置底部任务栏图标和左上角同步
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("myappid")
+        self.get_serials_name()
+        # -----------tablewidget表格中自定义下拉填充-----------
+        self.main_ui.combox_compress = QtWidgets.QComboBox()
+        self.main_ui.combox_compress.addItems(['on', 'off'])
+        self.main_ui.tableWidget.setCellWidget(0, 1, self.main_ui.combox_compress)
+        self.main_ui.combox_interleave = QtWidgets.QComboBox()
+        self.main_ui.combox_interleave.addItems(['off', 'on'])
+        self.main_ui.tableWidget.setCellWidget(1, 1, self.main_ui.combox_interleave)
+        self.main_ui.combox_ccnum = QtWidgets.QComboBox()
+        self.main_ui.combox_ccnum.setEditable(True)  # 设置下拉box可配
+        self.main_ui.combox_ccnum.addItems(['1', '2', '3'])
+        self.main_ui.tableWidget.setCellWidget(2, 1, self.main_ui.combox_ccnum)
+        self.main_ui.combox_ccnum.currentTextChanged[str].connect(self.carrier_cfg_tablewidget)
+        # # -----------生成载波采样率下拉按钮---------------------
+        # self.main_ui.comboBox_msps = QtWidgets.QComboBox()
+        # x = 7.68
+        # listx = []
+        # for i in range(1, 17):
+        #     listx.append(str('{0:.2f}'.format(x * i)))
+        # self.main_ui.comboBox_msps.addItems(listx)
+        # self.main_ui.tableWidget.setCellWidget(2, 1, self.main_ui.comboBox_msps)
+        # -------------窗口打印部件槽连接-----------------------
+        self.infoMsgPrint.connect(self.information_print)
+        self.operMsgPrint.connect(self.operation_print)
+        # -------------按钮连接槽函数--------------------------
+        self.main_ui.connnect_button.clicked.connect(self.connect)
+        self.main_ui.disconnect_button.clicked.connect(self.disconnect)
+        self.main_ui.cfgfile_button.clicked.connect(self.cfgfile_button_clicked)
+        self.main_ui.btnCfgSend.clicked.connect(self.get_script_cfg)
+        self.main_ui.menuhelp.addAction("&About",self.about)
+        self.main_ui.pushButton_save.clicked.connect(self.save_dialog)
+        self.main_ui.pushButton.clicked.connect(self.cfgByName)
+        self.main_ui.pushButtonDelete.clicked.connect(self.delByName)
+        self.main_ui.pushButton_3.clicked.connect(self.infoByScriptName)
+        self.main_ui.pushButton_download.clicked.connect(self.tftpd_32_open)
+        self.main_ui.pushButton_openFile.clicked.connect(self.getFirmwareFile)
+        self.read_yaml()#读取自定义的脚本
+        l = QtWidgets.QVBoxLayout(self.main_ui.widget2)
+        sc = MyStaticMplCanvas(self.main_ui.widget2, width=5, height=4, dpi=100)
+        l.addWidget(sc)
+
+    # DFE数量下拉值改变触发的槽函数
+    def carrier_cfg_tablewidget(self, cc_num):
+        self.main_ui.tableWidget_2.clearContents()
+        carrierCfg = self.get_cfg()
+        a = int(cc_num)
+        for i in range(a):
+            chCC = str(i+1)
+            item = QTableWidgetItem(chCC)
+            self.main_ui.tableWidget_2.setItem(i, 0, item)
+            item = QTableWidgetItem(carrierCfg[0])
+            self.main_ui.tableWidget_2.setItem(i, 1, item)
+            item = QTableWidgetItem(carrierCfg[3])
+            self.main_ui.tableWidget_2.setItem(i, 2, item)
+            item = QTableWidgetItem('1')
+            self.main_ui.tableWidget_2.setItem(i, 3, item)
+            item = QTableWidgetItem('0')
+            self.main_ui.tableWidget_2.setItem(i, 4, item)
+            item = QTableWidgetItem('0')
+            self.main_ui.tableWidget_2.setItem(i, 5, item)
+
+    #获取UI界面参数进行配置
+    def get_script_cfg(self):
+        compress=self.main_ui.combox_compress.currentText()
+        interleave=self.main_ui.combox_interleave.currentText()
+        # 获取ccnum按钮的内容来判断要读取表格的行数
+        rowCount = int(self.main_ui.combox_ccnum.currentText())
+        data = []
+        for i in range(rowCount):
+            dictMsg ={}
+            chcfg = []
+            for j in range(6):
+                cellItem = self.main_ui.tableWidget_2.item(i, j).text()
+                chcfg.append(cellItem)
+            dictMsg['msps']=chcfg[1]
+            dictMsg['width']=chcfg[2]
+            dictMsg['ch_num'] = chcfg[3]
+            dictMsg['ch_cc']=chcfg[0]
+            dictMsg['ant_num']=chcfg[4]
+            dictMsg['offset'] =chcfg[5]
+            data.append(dictMsg)
+        sendData = {"compress":compress,"interleave":interleave,"ch_num":rowCount,"data":data}
+        self.ser_cfg_send(sendData)
+
+    # 配置下发
+    def ser_cfg_send(self,dictMsg):
+        cpriConfigMsg = f"cpri reset\ncpri compress {dictMsg['compress']}\ncpri interleave {dictMsg['interleave']}\n"
+        # 获取ccnum按钮的内容来判断要读取表格的行数
+        rowCount = dictMsg['ch_num']
+        data = dictMsg['data']
+        for i in range(rowCount):
+            addcc = f"addcc   {data[i]['msps']}    {data[i]['width']}       {data[i]['ch_num']}            {data[i]['ch_cc']}             {data[i]['ant_num']}           {data[i]['offset']}\n"
+            cpriConfigMsg = cpriConfigMsg + addcc
+        cpri_dfe_enable = 'cpri enable\ndfe enable\n'
+        filename = datetime.now().strftime('%Y%m%d%H%M%S') + '.config'
+        cpriConfigMsg = f'echo -e "{cpriConfigMsg}{cpri_dfe_enable}" >>/lib/firmware/{filename}'
+        commond = f"dfe -f /lib/firmware/{filename}"
+        self.seial_msgs_send(cpriConfigMsg)
+        self.main_ui.plainTextEdit.clear()
+        sleep(0.2)
+        self.seial_msgs_send(commond)
+
+    # 获取界面参数返回值
+    def get_cfg(self):
+        # msps = self.main_ui.comboBox_msps.currentText()
+        compressCfg = self.main_ui.combox_compress.currentText()
+        interleaveCfg = self.main_ui.combox_interleave.currentText()
+        widthCfg = {'on': '8', 'off': '16'}
+        width = widthCfg[compressCfg]
+        list_resp = ['7.68', compressCfg, interleaveCfg, width]
+        return list_resp
+
+    #填写保存界面
+    def save_dialog(self):
+        self.saveDialog = QDialog()
+        self.saveDialog.setWindowTitle("保存配置名")
+        self.saveDialog.setWindowIcon(QIcon("./image/ICT.ico"))
+        self.saveDialog.okbtn = QPushButton("保存")
+        self.saveDialog.lineEdit = QLineEdit()
+        layout = QFormLayout(self.saveDialog)
+        layout.addRow(self.saveDialog.lineEdit,self.saveDialog.okbtn)
+        self.saveDialog.okbtn.clicked.connect(self.save_cfg)
+        self.saveDialog.exec_()
+
+    #获取当前配置并保存至config.yaml配置文件
+    def save_cfg(self):
+        fileName = self.saveDialog.lineEdit.text()
+        if fileName:
+            compress=self.main_ui.combox_compress.currentText()
+            interleave=self.main_ui.combox_interleave.currentText()
+            # 获取ccnum按钮的内容来判断要读取表格的行数
+            ccnum = int(self.main_ui.combox_ccnum.currentText())
+            data_list = [] #存放DFE载波具体信息
+            for i in range(ccnum):
+                chcfg = []
+                for j in range(6):
+                    cellItem = self.main_ui.tableWidget_2.item(i, j).text()
+                    chcfg.append(cellItem)
+                data = {"ch_cc":chcfg[0],
+                        "msps":chcfg[1],
+                        "width":chcfg[2],
+                        "ch_num":chcfg[3],
+                        "ant_num":chcfg[4],
+                        "offset":chcfg[5]
+                        }
+                data_list.append(data)
+            saveData = {
+            "compress": compress,
+            "interleave": interleave,
+            "ch_num":ccnum,
+            "data": data_list
+            }
+            self.dictScript[fileName] = saveData
+            self.write_yaml(self.dictScript)#将文件名和配置信息写入yaml文件中
+            self.saveDialog.close()
+        else:
+            QMessageBox.information(self.saveDialog,"提示","配置名不能为空")
+
+    #导入配置生成对应的yaml文件
+    def write_yaml(self,dictData):
+        with open("./config/testCases.yaml", encoding='utf-8', mode='w') as f:
+            try:
+                dump_all(documents=[dictData], stream=f, allow_unicode=True)
+            except Exception as e:
+                QMessageBox.information(self,"错误提示",e)
+        self.main_ui.comboBox_2.clear()
+        self.read_yaml()
+
+    #读取yaml文件并返回
+    def read_yaml(self):
+        with open(f"./config/testCases.yaml", encoding='utf-8') as f:
+            data = load(f.read(), Loader=FullLoader)
+            self.dictScript = data
+        boxList = []
+        for k in self.dictScript.keys():
+            boxList.append(k)
+        self.main_ui.comboBox_2.addItems(boxList)
+
+    #根据脚本名下发配置
+    def cfgByName(self):
+        try:
+            scriptName = self.main_ui.comboBox_2.currentText()
+            data = self.dictScript[scriptName]
+            self.ser_cfg_send(data)
+        except:
+            self.operMsgPrint.emit("当前不存在脚本")
+
+    #根据脚本名删除配置
+    def delByName(self):
+        try:
+            scriptName = self.main_ui.comboBox_2.currentText()
+            del self.dictScript[scriptName] #删除键值对
+            self.write_yaml(self.dictScript)#重新写入yaml配置文件
+        except:
+            self.operMsgPrint.emit("当前不存在脚本")
+
+    #根据脚本查看
+    def infoByScriptName(self):
+        try:
+            scriptName = self.main_ui.comboBox_2.currentText()
+            data = self.dictScript[scriptName]
+            self.operMsgPrint.emit(str(data))
+        except:
+            self.operMsgPrint.emit("当前不存在脚本")
+
+    # 获取当前设备连接的COM列表
+    def get_serials_name(self):
+        ports_get = list(serial.tools.list_ports.comports())
+        port_list = []
+        if len(ports_get) == 0:
+            self.operMsgPrint.emit("错误提示,找不到串口\n")
+        else:
+            for i in range(0, len(ports_get)):
+                port = (ports_get[i])
+                port_list.append(port[0])
+            port_list.sort()
+            self.main_ui.comboBox.addItems(port_list)
+
+    # 打开tftpd程序
+    def tftpd_32_open(self):
+        try:
+            tftpath = os.path.dirname(os.path.realpath(__file__))
+            app = "tftpd32.exe"
+            tftpath = f"{tftpath}\config\{app}"
+            open_app(tftpath)
+        except:
+            pass
+
+    #打开文件夹填充至lineEdit_2
+    def getFirmwareFile(self):
+        directory = QFileDialog.getExistingDirectory(self,"选取文件夹","./")
+        self.main_ui.lineEdit_2.setText(directory)
+
+    # 连接按钮槽函数
+    def connect(self):
+        self.serialThread = threading.Thread(target=self.serial_connect)
+        self.serialThread.setDaemon(True)  # 设置守护进程
+        self.serialThread.start()
+
+    # 断开连接槽函数
+    def disconnect(self):
+        global flagConnect
+        self.main_ui.connnect_button.setEnabled(True)  # 设置连接按钮可点击
+        flagConnect = False
+        try:
+            if self.serialThread.isAlive():
+                self.serialSingle.close()
+                self.operMsgPrint.emit(f"{self.main_ui.comboBox.currentText()}串口已经断开")
+        except:
+            self.operMsgPrint.emit("请先配置连接")
+
+    # 串口连接
+    def serial_connect(self):
+        global flagConnect
+        try:
+            self.serialSingle = serial.Serial(self.main_ui.comboBox.currentText(), 115200, timeout=0.5)
+            self.operMsgPrint.emit(f"{self.main_ui.comboBox.currentText()}串口已经连接")
+            flagConnect = True
+            self.main_ui.connnect_button.setEnabled(False)  # 设置连接按钮不可点击
+            while flagConnect:
+                data = self.recv(self.serialSingle)
+                if data != b'':
+                    try:
+                        serialMsg = str(data, encoding='utf-8')
+                        self.infoMsgPrint.emit(serialMsg)
+                        self.check_in(serialMsg)
+                    except:
+                        pass
+        except:
+            self.operMsgPrint.emit("注意：串口被占用或者不存在\n")
+
+    # 匹配关键字进行登陆操作
+    def check_in(self, strMsg):
+        global logginNum
+        strRegex1 = re.compile('login|success')
+        mo1 = strRegex1.search(strMsg)
+        if mo1.group() == 'login':
+            self.logThread = threading.Thread(target=self.logging)
+            self.logThread.setDaemon(True)
+            self.logThread.start()
+            logginNum += 1
+        if mo1.group() == 'success' and logginNum != 0: #判断loggingNum不为0，防止启动时打印也会提示脚本下发成功
+            self.operMsgPrint.emit("提示: 脚本下发成功")
+
+    # 进行串口登录操作，登陆完清除面板内容
+    def logging(self):
+        msgSend = ['root', 'ict']
+        for i in msgSend:
+            self.seial_msgs_send(i)
+            sleep(2)
+        self.main_ui.plainTextEdit.clear()
+        self.cfgfile_button_clicked()
+        self.operMsgPrint.emit("提示:芯片启动完成，可以进行配置")
+
+    # 实现串口已连接的serial监听，返回byte数据
+    def recv(self, serial_id):
+        while flagConnect:
+            try:
+                data = serial_id.read_all()
+                return data
+            except:
+                pass
+
+    # 串口信息打印
+    def information_print(self, mypstr):
+        self.main_ui.plainTextEdit.appendPlainText(mypstr.strip())
+
+    # 操作信息打印
+    def operation_print(self, mypstr):
+        currenTime = self.getCurrenTime()
+        self.main_ui.textEdit_2.append(currenTime + mypstr)
+
+    # 实现给已连接串口发送字符串数据
+    def cfgfile_button_clicked(self):
+        if flagConnect == True:
+            msgSend = self.main_ui.lineEdit_2.text() + '\n'
+            self.serialSingle.write(bytes(msgSend, encoding='utf-8'))
+        else:
+            self.operMsgPrint.emit("串口未连接！")
+
+    # 实现给已连接串口发送字符串数据
+    def seial_msgs_send(self, msgStr):
+        if flagConnect == True:
+            msgSend = msgStr + '\n'
+            self.serialSingle.write(bytes(msgSend, encoding='utf-8'))
+        else:
+            self.operMsgPrint.emit("串口未连接！")
+
+    # 返回当前时间年月日时分秒
+    def getCurrenTime(self):
+        localtime = datetime.now().strftime('%Y-%m-%d %H:%M:%S ')
+        return localtime
+
+    #软件版权信息
+    def about(self):
+        QMessageBox.about(self, "About",
+                                    """Copyright 2021 @创芯慧联 zhangbingxue"""
+                                )
+
+def open_app(app_dir):
+    os.startfile(app_dir)
+
+if __name__ == "__main__":
+    myapp = QApplication(sys.argv)
+    Main_Ui = IctTest()
+    Main_Ui.show()
+    sys.exit(myapp.exec_())
---
+++ test.py
@@ -0,0 +1,57 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+class MainWindow(QMainWindow):
+    count=0
+    def __init__(self,parent=None):
+        super(MainWindow, self).__init__(parent)
+        #实例化Qmidarea区域
+        self.mdi=QMdiArea()
+        #设置为中间控件
+        self.setCentralWidget(self.mdi)
+
+        #实例化菜单栏
+        bar=self.menuBar()
+        #添加主菜单
+        file=bar.addMenu('File')
+        #添加子菜单
+        file.addAction('New')
+        file.addAction('cascade')
+        file.addAction('Tiled')
+
+        #点击QAction绑定自定义的槽函数（传递有值【QAction】）
+        file.triggered[QAction].connect(self.windowaction)
+        #设置主窗口的标题
+        self.setWindowTitle("MDI demo")
+    def windowaction(self,q):
+        print('Triggered')
+
+        if q.text()=='New':
+            #子窗口增加一个
+            MainWindow.count=MainWindow.count+1
+
+            #实例化多文档界面对象
+            sub=QMdiSubWindow()
+            #向sub内添加内部控件
+            sub.setWidget(QTextEdit())
+            #设置新建子窗口的标题
+            sub.setWindowTitle('subWindow'+str(MainWindow.count))
+
+            #将子窗口添加到Mdi区域
+            self.mdi.addSubWindow(sub)
+
+            #子窗口显示
+            sub.show()
+        if q.text()=='cascade':
+            #cascadeSubWindows()：安排子窗口在Mdi区域级联显示
+            self.mdi.cascadeSubWindows()
+        if q.text()=='Tiled':
+            #tileSubWindow():安排子窗口在Mdi区域平铺显示
+            self.mdi.tileSubWindow()
+if __name__ == '__main__':
+    app=QApplication(sys.argv)
+    demo=MainWindow()
+    demo.show()
+    sys.exit(app.exec_())
